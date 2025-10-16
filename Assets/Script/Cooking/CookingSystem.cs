078032108aba05535b388337bb8463cc7c82023f
bryan lee <bryan900926@gmail.com>
2025-10-16 18:14:44 +0800

    remove the old cooking logic

--- Assets/Script/Cooking/CookingSystem.cs
+++ Assets/Script/Cooking/CookingSystem.cs
@@ -1,49 +1,49 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class CookingSystem : MonoBehaviour
-{
-    private HoldingSystem holdingSystem;
-    private RecipeSystem recipeSystem;
+// public class CookingSystem : MonoBehaviour
+// {
+//     private HoldingSystem holdingSystem;
+//     private RecipeSystem recipeSystem;
 
-    [SerializeField] private GameObject counter;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        holdingSystem = GameObject.FindGameObjectWithTag("HoldingSystem").GetComponent<HoldingSystem>();
-        recipeSystem = RecipeSystem.Instance;
-    }
+//     [SerializeField] private GameObject counter;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         holdingSystem = GameObject.FindGameObjectWithTag("HoldingSystem").GetComponent<HoldingSystem>();
+//         recipeSystem = RecipeSystem.Instance;
+//     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!other.CompareTag("Player")) return;
-        MakingFood();
+//     private void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if (!other.CompareTag("Player")) return;
+//         MakingFood();
 
-    }
+//     }
 
-    private void MakingFood()
-    {
-        int mask = 0;
-        // masks of each fooditem are at RecipeSystem.cs
-        foreach (GameObject food in holdingSystem.slots)
-        {
-            IngredientData ingredient = food.GetComponent<HoldingSlot>().currentItem;
-            if (ingredient != null)
-            {
-                mask |= ingredient.Mask;
-            }
-        }
+//     private void MakingFood()
+//     {
+//         int mask = 0;
+//         // masks of each fooditem are at RecipeSystem.cs
+//         foreach (GameObject food in holdingSystem.slots)
+//         {
+//             IngredientData ingredient = food.GetComponent<HoldingSlot>().currentItem;
+//             if (ingredient != null)
+//             {
+//                 mask |= ingredient.Mask;
+//             }
+//         }
 
-        int res = recipeSystem.Match(mask);
+//         int res = recipeSystem.Match(mask);
 
-        if (res != -1)
-        {
-            Menu menu = Menu.Instance;
-            foreach (GameObject food in holdingSystem.slots)
-            {
-                HoldingSlot slot = food.GetComponent<HoldingSlot>();
-                slot.removeItem();
-            }
-            menu.SpawnForPlayer(res, counter.transform.position);
-        }
-    }
-}
+//         if (res != -1)
+//         {
+//             Menu menu = Menu.Instance;
+//             foreach (GameObject food in holdingSystem.slots)
+//             {
+//                 HoldingSlot slot = food.GetComponent<HoldingSlot>();
+//                 slot.removeItem();
+//             }
+//             menu.SpawnForPlayer(res, counter.transform.position);
+//         }
+//     }
+// }
